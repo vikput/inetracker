224383825742c76fdde506ae8f66e010c798d6ab
vikas <vikasrsingh579@gmail.com>
2020-12-13 15:21:31 +0530

    Feature :- Added incomesources listing.

--- services/incomesService.js
+++ services/incomesService.js
@@ -19,7 +19,30 @@ exports.getUsersIncomeSources = async function(data){
 	Check if income source is already exists
 */
 exports.checkIncomeSource = async function(data){
-	return await incSModel.checkIncomeSource(data)
+	return await incSModel.checkIncomeSource(data);
+}
+
+/**
+   Income sources list view
+*/
+exports.getUsersIncomeSourcesListView = async function(data, start, limit, search, sortBy, orderBy){
+    results = await incSModel.getUsersIncomeSourcesListView(data, start, limit, search, sortBy, orderBy);
+    incSourceData = [];
+    for(let i=0; i<results.length; i++){
+    	incSourceData.push({
+    		'income_sources': results[i].users_income_sources,
+    		'action': 'Action'
+    	}); 
+    }
+    return incSourceData;
+}
+
+/**
+   Get total records count
+*/
+exports.getUsersTotalIncScount = async function(data){
+	results =  await incSModel.getUsersTotalIncScount(data);
+	return results.data;
 }
 
 /*
