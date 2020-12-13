9ef3c77655e214ee677ae4ab8cb9a11521b5aa61
vikas <vikasrsingh579@gmail.com>
2020-12-13 19:04:23 +0530

    Featur: Added expenses listing.

--- models/expensesModel.js
+++ models/expensesModel.js
@@ -25,3 +25,59 @@ exports.saveData = function(expenseData){
 	    conn.end();
 	});
 }
+
+exports.fetchUsersExpenses = function(data, start, limit, sortByArr, orderByArr){
+  return new Promise(function(resolve, reject){
+    let dbObj = new mysqldb();
+    let conn = dbObj.connect();
+    conn.connect();
+    let orderByArray = ['ex_year', 'ex_month'];
+    let response = {};
+    let query = 'SELECT ex.id AS id, ex.ex_year AS `year`, ex.ex_month AS `month`, ex.ex_date AS `date`, ex.ex_amount AS amount, ex.ex_comment AS comments, incs.users_income_sources AS income_sources FROM users_monthly_expenses AS ex, users_income_sources AS incs WHERE ex.user_id = ? AND ex.uis_id=incs.id';
+    query += ' ORDER BY ';
+    for(let i=0; i<sortByArr.length; i++){
+        query+= orderByArray[sortByArr[i]]+' '+orderByArr[sortByArr[i]];
+        if (i < (sortByArr.length - 1)){
+        	query +=' ,';
+        }
+    }
+    query += ' LIMIT '+ start+', '+limit;
+    conn.query(query, data, function(err, result){
+        if (err) {
+          response.status = configObj.error.status;
+              response.message = configObj.error.err1_message;
+              //response.message = err;
+              response.data = '';
+              reject(response);
+        } else {
+          resolve(result);
+        }
+      });
+    conn.end();
+  });
+}
+
+exports.getUsersTotalExpCount = function(data){
+  return new Promise(function(resolve, reject){
+    let dbObj = new mysqldb();
+        let conn = dbObj.connect();
+        conn.connect();
+        let response = {};
+        let query = 'SELECT COUNT(id) as ex_count FROM users_monthly_expenses WHERE user_id=?';
+        conn.query(query, data, function(err, result){
+          if (err) {
+                response.status = configObj.error.status;
+              response.message = configObj.error.err1_message;
+              //response.message = err;
+              response.data = '';
+              reject(response);
+          } else {
+            response.status = configObj.success.status;
+                response.message = '';
+                response.data = result[0].ex_count;
+                resolve(response);
+          }
+        });
+        conn.end();
+  });
+}
--- services/expensesService.js
+++ services/expensesService.js
@@ -5,3 +5,31 @@ exports._saveData = async function(data){
 	console.log('service')
 	return await expModel.saveData(data);
 }
+
+/**
+   Get total records count
+*/
+exports.getUsersTotalExpCount = async function(data){
+    results =  await expModel.getUsersTotalExpCount(data);
+    return results.data;
+}
+
+/**
+    Fetch users monthly income data
+*/
+exports.fetchUsersExpenses = async function(data, start, limit, sortByArr, orderByArr){
+    results = await expModel.fetchUsersExpenses(data, start, limit, sortByArr, orderByArr);
+    expenses = [];
+    for (let i=0; i<results.length; i++){
+        expenses.push({
+            'year': results[i].year,
+            'month': results[i].month,
+            'date': results[i].date,
+            'amount': results[i].amount,
+            'comments': results[i].comments,
+            'action': 'Action'
+        });
+    }
+
+    return expenses;
+}
